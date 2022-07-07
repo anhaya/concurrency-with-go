c7a3d70c960f45423b615bf50af5bead24673131
CARLOS ANDRE ANHAYA DE BARROS <carlos.anhaya@gmail.com>
2022-07-07 12:30:30 -0300

    the or channel

---
+++ orchannel/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	var or func(channels ...<-chan interface{}) <-chan interface{}
+	or = func(channels ...<-chan interface{}) <-chan interface{} {
+		switch len(channels) {
+		case 0:
+			return nil
+		case 1:
+			return channels[0]
+		}
+		orDone := make(chan interface{})
+		go func() {
+			defer close(orDone)
+			select {
+			case <-channels[0]:
+			case <-or(append(channels[1:], orDone)...):
+			}
+
+		}()
+		return orDone
+	}
+
+	sig := func(after time.Duration) <-chan interface{} {
+		c := make(chan interface{})
+		go func() {
+			defer close(c)
+			time.Sleep(after)
+		}()
+		return c
+	}
+	start := time.Now()
+	<-or(
+		sig(2*time.Hour),
+		sig(5*time.Minute),
+		sig(1*time.Minute),
+		sig(1*time.Hour),
+		sig(1*time.Second),
+	)
+	fmt.Printf("done after %v", time.Since(start))
+}
